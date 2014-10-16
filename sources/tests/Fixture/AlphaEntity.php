5fd0010b0e5e9b216779b5fe9c5804c5d9e49aa8
Grégoire HUBERT <hubert.greg@gmail.com>
2014-10-16 15:53:31 +0200

    Test for generate:entity.

---
+++ sources/tests/Fixture/AlphaEntity.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Model\PommTest\PommTestSchema;
+
+use PommProject\ModelManager\Model\FlexibleEntity;
+
+/**
+ * Alpha
+ *
+ * Flexible entity for relation
+ * pomm_test.alpha
+ *
+ * @see FlexibleEntity
+ */
+class Alpha extends FlexibleEntity
+{
+}
