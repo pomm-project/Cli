a76bc8c3fe412ef744ed8236b10347509a18229e
Grégoire HUBERT <hubert.greg@gmail.com>
2019-08-14 22:57:01 +0200

    fix path-pattern with model namespace

--- sources/lib/Command/GenerateRelationModel.php
+++ sources/lib/Command/GenerateRelationModel.php
@@ -83,7 +83,7 @@ class GenerateRelationModel extends RelationAwareCommand
             $input->getOption('psr4'),
             $input->getOption('path-pattern')
         );
-        $this->namespace = $this->getNamespace($input->getArgument('config-name'));
+        $this->namespace = $this->getNamespace($input->getArgument('config-name'), null, $input->getOption('path-pattern'));
 
         $this->updateOutput(
             $output,
